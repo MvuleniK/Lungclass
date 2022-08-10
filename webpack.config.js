bc78fd254bba47b2b57bba36ce866b467a4a22c1
MvuleniK <99172858+MvuleniK@users.noreply.github.com>
2022-08-10 09:45:50 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+// module.exports = async function (env, argv) {
+//   const config = await createExpoWebpackConfigAsync(env, argv);
+//   // Customize the config before returning it.
+//   return config;
+// };
+
+
+
+
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function(env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+      {
+        ...env,
+        babel: {
+          dangerouslyAddModulePathsToTranspile: [
+            // Ensure that all packages starting with @tensorflow are
+            // transpiled.
+            '@tensorflow',
+          ],
+        },
+      },
+      argv);
+  return config;
+};
+
+
